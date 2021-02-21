66bc8d77f34ae4c36679f69147f2e5f2966c2e46
SwipSwup <davidveigel.business@gmail.com>
2021-02-21 19:47:17 +0100

    tmp

--- src/js/Fetch.js
+++ src/js/Fetch.js
@@ -1,7 +1,15 @@
-export class Fetch {
+class Fetch {
      constructor(url) {
+         let tmp;
         fetch(url)
-            .then(response => response.json())
+            .then(url => url.json())
             .then(data => console.log(data))
+            .catch(console.error(url + ' is an invalid url'));
+        this.data = tmp;
+    }
+
+    getJsonArray() {
+         console.log(this.data);
+         return JSON.stringify(this.data);
     }
 }
